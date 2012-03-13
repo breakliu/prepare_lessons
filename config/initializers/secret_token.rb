a45220665b0cf244ecb44425648e0a4aa77edf07
slackcode <slackcode@gmail.com>
2012-03-13 10:18:03 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrepareLessons::Application.config.secret_token = '7608e4ce2e36530a401842fc16d71ed787c48e97bc7db438398396147e52fed6c68d113408c405ea9bcf9e4b5be11945fdf7c3d4d4dc1a342cc747a546caf506'
